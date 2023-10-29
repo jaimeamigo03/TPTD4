0381ad8df428771ee38cfba29c5cf3204f4da1d1
jaimeamigo03 <102691435+jaimeamigo03@users.noreply.github.com>
2023-10-29 20:34:11 -0300

    Update traceroute.py

--- traceroute.py
+++ traceroute.py
@@ -1,8 +1,3 @@
-from scapy.all import *
-import socket
-import time
-
-# TraceRoute (Ejercicio 1)
 def traceRoute(destination):
     TimeToLive = 1
     maxHops = 30
@@ -10,29 +5,26 @@ def traceRoute(destination):
 
     print("Destination IP: {hostIP}".format(hostIP=hostIP))
 
-    while TimeToLive <= maxHops:  # Adjust the loop condition to include maxHops
-        # Create the ICMP Echo Request packet with TTL
+    while TimeToLive <= maxHops:  # Ciclo para modificar el TTL de cada paquete
+        
+        # Creamos paquete con el TTL correspondiente 
         packet = IP(dst=destination, ttl=TimeToLive) / ICMP(type=8, code=0)
 
-        # Capture the packet and record the timestamp before sending
+        # Iniciamos el tiempo para calcular el RTT
         start_time = time.time()
         reply = sr1(packet, verbose=0, timeout=1)
 
         if reply is None:
-            # If there is no response, print '*' and calculate the time
+            # Si no hay respuesta, imprimimos "*" y calculamos el tiempo 
             end_time = time.time()
             print(f"[{TimeToLive}] * (Time: {int((end_time - start_time) * 1000)} ms)")
         else:
-            # If a response is received, print the source IP and calculate the time
+            # Si hay respuesta, imprimimos la IP source y calculamos el tiempo 
             end_time = time.time()
             print(f"[{TimeToLive}] {reply.src} (Time: {int((end_time - start_time) * 1000)} ms)")
 
-            # Stop when we reach the destination
+            # Rompemos el ciclo si llegamos a la IP deseada
             if reply.src == hostIP:
                 break
 
         TimeToLive += 1
-
-if __name__ == "__main__":
-    destination = input("Destino: ")
-    traceRoute(destination)
